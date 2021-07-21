5e0ddbaf5dd3561f1ca6977b93732979bf52f1cb
Arcanjo <agr.arcanjo@gmail.com>
2021-07-21 13:54:35 -0300

    projeto playflow flutter

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/social_login/social_login_button.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  LoginPage({Key? key}) : super(key: key);
 
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-        backgroundColor: AppColors.background,
-        body: Container(
-          width: size.width,
-          height: size.height,
-          child: Stack(children: [
+      backgroundColor: AppColors.background,
+      body: Container(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          children: [
             Container(
               width: size.width,
-              height: size.height * 0.15,
+              height: size.height * 0.36,
               color: AppColors.primary,
             ),
             Positioned(
@@ -31,11 +36,11 @@ class _LoginPageState extends State<LoginPage> {
                 right: 0,
                 child: Image.asset(
                   AppImages.person,
-                  width: 200,
+                  width: 208,
                   height: 300,
                 )),
             Positioned(
-              bottom: size.height * 0.18,
+              bottom: size.height * 0.05,
               left: 0,
               right: 0,
               child: Column(
@@ -48,14 +53,25 @@ class _LoginPageState extends State<LoginPage> {
                         const EdgeInsets.only(top: 30, left: 70, right: 70),
                     child: Text(
                       "Organize seus boletos em um só lugar",
-                      style: AppTextStyles.titleHome,
                       textAlign: TextAlign.center,
+                      style: TextStyles.titleHome,
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 40, right: 40, top: 40),
+                    child: SocialLoginButton(
+                      onTap: () {
+                        controller.googleSignIn(context);
+                      },
                     ),
                   )
                 ],
               ),
             )
-          ]),
-        ));
+          ],
+        ),
+      ),
+    );
   }
 }
